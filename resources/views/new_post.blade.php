7ece0a4874c436c4ef8c4b7890276685d8b8d8b2
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-04-11 21:54:06 +0300

    add new migration

--- resources/views/new_post.blade.php
+++ resources/views/new_post.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <div class="container-sm">
             <h2>New Post</h2>
-            <form action="" method="POST">
+            <form action="" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
                     <input class="form-control" type="text" name="title" placeholder="Title here...">
@@ -13,6 +13,9 @@
                 </div>
                 <div class="mb-3">
                     <textarea class="form-control" name="body" rows="10" cols="30"></textarea>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control" type="file" name="photo">
                 </div>
                 <button class="btn btn-primary mb-3" type="submit">Submit</button>
             </form>
