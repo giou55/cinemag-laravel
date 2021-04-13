07bb2de3aa4ce9d97ec21ace5ebb58adeb62d322
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-04-14 00:01:14 +0300

    add header links

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container" id="header">
+        <div class="d-flex justify-content-between align-items-center links-1">
+            <div>LIFO</div><div class="divider"></div>
+            <div>IN.GR</div><div class="divider"></div>
+            <div>ZOUGLA</div><div class="divider"></div>
+            <div>NETFLIX</div><div class="divider"></div>
+            <div>FACEBOOK</div>
+        </div>
+            
+        <div style="font-size: 1.8rem; font-family:'Times New Roman', Times, serif;">Athens</div>
+
+        <div class="d-flex justify-content-between align-items-center links-1">
+            <div>CINEMAGAZINE</div><div class="divider"></div>
+            <div>ATHINORAMA</div>
+            
+            <div class="d-flex justify-content-between align-items-center links-2">
+                <div><strong>Subscribe </strong></div><div class="divider"></div>
+                <div><strong> Sign In</strong></div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,11 +21,11 @@
 </head>
 <body>
     <div id="app">
+        @include('includes.header')
+
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="#">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -34,7 +34,22 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">ΣΙΝΕΜΑ</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">ΘΕΑΤΡΟ</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">ΜΟΥΣΙΚΗ</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">YOUTUBE</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">ΒΙΒΛΙΑ</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">PODCASTS</a>
                         </li>
                         <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('posts') }}">Posts</a>
