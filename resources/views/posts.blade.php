c856a397c3c5ae806ff4064f8064214024486869
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-04-25 10:25:36 +0300

    change layout in posts admin page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,9 +3,10 @@
 @section('content')
         <div class="container-sm">
             <h2>Τα άρθρα μου</h2>
-            <div class="row">
+            {{-- <div class="d-flex flex-row flex-wrap"> --}}
+            <div class="card-columns">
                 @foreach ($posts as $post)
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                    {{-- <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                         <div class="mb-3 card">
                         @if ($post->image)
                         <img src="/storage/post_images/{{ $post->image }}" class="card-img-top" alt="">
@@ -18,6 +19,17 @@
                             <p class="card-text">Συντάκτης: {{ $post->user->fullname }}</p>
                             <p class="card-text">{{ $post->body }}</p>
                         </div>
+                        </div>
+                    </div> --}}
+                    <div class="card">
+                        @if ($post->image)
+                            <img class="card-img-top" src="/storage/post_images/{{ $post->image }}" alt="">
+                        @endif
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="{{ route('post', $post) }}">{{ $post->title }}</a>
+                            </h5>
+                            <p class="card-text">{{ $post->body }}</p>
                         </div>
                     </div>
                 @endforeach
